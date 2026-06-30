53033b9250f09e1c4188c84d1738fc9608dfb8e3
Frederico D. Sasek <fsasek6@gmail.com>
2026-06-30 20:35:11 -0300

    adicção do evento click com correção de estilos

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 const a = [8, 7, 6, 5, 4, 3, 2, 1];
 const b = [];
 const c = [];
+const move = 0;
+
+const moviment = document.querySelector("#move");
+
+const torre1 = document.querySelector("#tower-1");
+const torre2 = document.querySelector("#tower-2");
+const torre3 = document.querySelector("#tower-3");
+torre1.addEventListener("click", mudavalor1);
+torre2.addEventListener("click", mudavalor2);
+torre3.addEventListener("click", mudavalor3);
+
+function mudavalor1() {
+  moviment.textContent = "a";
+}
+
+function mudavalor2() {
+  moviment.textContent = "b";
+}
+
+function mudavalor3() {
+  moviment.textContent = "c";
+}
 
 function moverTorre(t1, t2) {
   const tam1 = t1.length;
@@ -9,5 +31,7 @@ function moverTorre(t1, t2) {
     //jogada invalida
   } else {
     t2.push(t1.pop());
+    move = move + 1;
+    moviment.textContent = move;
   }
 }
