cd8fea8327c64d67e536a7fcce2b3e06569f97b6
Frederico D. Sasek <fsasek6@gmail.com>
2026-07-01 13:53:54 -0300

    cria os primeiros discos do jogo

--- main.js
+++ main.js
@@ -2,34 +2,38 @@ const a = [8, 7, 6, 5, 4, 3, 2, 1];
 const b = [];
 const c = [];
 const move = 0;
-const historico  = document.querySelector("#historico");
 
+const historico = document.querySelector("#historico");
 const moviment = document.querySelector("#move");
 
 const torre1 = document.querySelector("#tower-1");
 const torre2 = document.querySelector("#tower-2");
 const torre3 = document.querySelector("#tower-3");
+const pole= document.querySelectorAll("#pole");
+
 torre1.addEventListener("click", render);
 torre2.addEventListener("click", render);
 torre3.addEventListener("click", render);
 
 function render() {
+  pole.innerHTML = " ";
+  console.log("pole limpo");
   for (let i = 0; i < a.length; i++) {
-    criaDisco(a[i]);
+    criaDisco(a[i], torre1);
   }
-    for (let i = 0; i < b.length; i++) {
-    criaDisco(b[i]);
+  for (let i = 0; i < b.length; i++) {
+    criaDisco(b[i], torre2);
   }
-    for (let i = 0; i < c.length; i++) {
-    criaDisco(c[i]);
+  for (let i = 0; i < c.length; i++) {
+    criaDisco(c[i], torre3);
   }
 }
 
-function criaDisco(disckSize) {
-  const name = "disk - " + disckSize;
-  const novoDisk = document.createElement("li");
-  novoDisk.textContent = name;
-
+function criaDisco(disckSize, torre) {
+  const name = "s" + disckSize;
+  const novoDisk = document.createElement("div");
+  novoDisk.className = name;
+  torre.append(novoDisk);
 }
 
 function moverDisco(t1, t2) {
