f2509b75c97fbfbb747a4ab508ed7137b545aa89
Frederico D. Sasek <fsasek6@gmail.com>
2026-07-01 18:27:19 -0300

    correção de posição dos discos e cncertando a renderização

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const a = [8, 7, 6, 5, 4, 3, 2, 1];
 const b = [];
 const c = [];
-const move = 0;
+let move = 0;
 
 const historico = document.querySelector("#historico");
 const moviment = document.querySelector("#move");
@@ -9,15 +9,27 @@ const moviment = document.querySelector("#move");
 const torre1 = document.querySelector("#tower-1");
 const torre2 = document.querySelector("#tower-2");
 const torre3 = document.querySelector("#tower-3");
-const pole= document.querySelectorAll("#pole");
+const pole = document.querySelectorAll(".pole");
 
 torre1.addEventListener("click", render);
 torre2.addEventListener("click", render);
 torre3.addEventListener("click", render);
 
+function iniciarJogo() {
+  move = 0;
+  moviment.textContent = move;
+
+  render();
+}
+
+function limpar() {
+  document.querySelectorAll(".tower span").forEach((disco) => {
+    disco.remove();
+  });
+}
+
 function render() {
-  pole.innerHTML = " ";
-  console.log("pole limpo");
+  limpar();
   for (let i = 0; i < a.length; i++) {
     criaDisco(a[i], torre1);
   }
@@ -31,7 +43,7 @@ function render() {
 
 function criaDisco(disckSize, torre) {
   const name = "s" + disckSize;
-  const novoDisk = document.createElement("div");
+  const novoDisk = document.createElement("span");
   novoDisk.className = name;
   torre.append(novoDisk);
 }
@@ -39,7 +51,7 @@ function criaDisco(disckSize, torre) {
 function moverDisco(t1, t2) {
   const tam1 = t1.length;
   const tam2 = t2.length;
-  if (tam2 > 0 && t2[tam2 - 1] > t1[tam1 - 1]) {
+  if (tam2 > 0 && t1[tam1 - 1] > t2[tam2 - 1]) {
     //jogada invalida
   } else {
     t2.push(t1.pop());
@@ -48,3 +60,6 @@ function moverDisco(t1, t2) {
   }
   render();
 }
+
+
+iniciarJogo();
