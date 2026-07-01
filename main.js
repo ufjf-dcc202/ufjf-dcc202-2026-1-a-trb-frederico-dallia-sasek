203ab206890ea1cb24b993af21e769cc6f14a48f
Frederico D. Sasek <fsasek6@gmail.com>
2026-07-01 13:31:50 -0300

    pequenos testes para ver como exibir no historico e renderizar os discos

--- main.js
+++ main.js
@@ -2,29 +2,37 @@ const a = [8, 7, 6, 5, 4, 3, 2, 1];
 const b = [];
 const c = [];
 const move = 0;
+const historico  = document.querySelector("#historico");
 
 const moviment = document.querySelector("#move");
 
 const torre1 = document.querySelector("#tower-1");
 const torre2 = document.querySelector("#tower-2");
 const torre3 = document.querySelector("#tower-3");
-torre1.addEventListener("click", mudavalor1);
-torre2.addEventListener("click", mudavalor2);
-torre3.addEventListener("click", mudavalor3);
+torre1.addEventListener("click", render);
+torre2.addEventListener("click", render);
+torre3.addEventListener("click", render);
 
-function mudavalor1() {
-  moviment.textContent = "a";
+function render() {
+  for (let i = 0; i < a.length; i++) {
+    criaDisco(a[i]);
+  }
+    for (let i = 0; i < b.length; i++) {
+    criaDisco(b[i]);
+  }
+    for (let i = 0; i < c.length; i++) {
+    criaDisco(c[i]);
+  }
 }
 
-function mudavalor2() {
-  moviment.textContent = "b";
+function criaDisco(disckSize) {
+  const name = "disk - " + disckSize;
+  const novoDisk = document.createElement("li");
+  novoDisk.textContent = name;
+
 }
 
-function mudavalor3() {
-  moviment.textContent = "c";
-}
-
-function moverTorre(t1, t2) {
+function moverDisco(t1, t2) {
   const tam1 = t1.length;
   const tam2 = t2.length;
   if (tam2 > 0 && t2[tam2 - 1] > t1[tam1 - 1]) {
@@ -34,4 +42,5 @@ function moverTorre(t1, t2) {
     move = move + 1;
     moviment.textContent = move;
   }
+  render();
 }
