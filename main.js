d2afd7042ad8a49ce69700b62e18f692606175a9
Frederico D. Sasek <fsasek6@gmail.com>
2026-07-02 13:12:07 -0300

    adiciona funcionalidade e jogabilidade, alem de endiretar muitas coisas da aparencia que incomodavam

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const a = [8, 7, 6, 5, 4, 3, 2, 1];
 const b = [];
 const c = [];
-let move = 0;
+let selecionado;
+let torreSelecionada;
+let move;
 
 const historico = document.querySelector("#historico");
 const moviment = document.querySelector("#move");
@@ -11,12 +13,13 @@ const torre2 = document.querySelector("#tower-2");
 const torre3 = document.querySelector("#tower-3");
 const pole = document.querySelectorAll(".pole");
 
-torre1.addEventListener("click", render);
-torre2.addEventListener("click", render);
-torre3.addEventListener("click", render);
+torre1.addEventListener("click", () => selecionar(a, torre1));
+torre2.addEventListener("click", () => selecionar(b, torre2));
+torre3.addEventListener("click", () => selecionar(c, torre3));
 
 function iniciarJogo() {
   move = 0;
+  selecionado = 0;
   moviment.textContent = move;
 
   render();
@@ -48,18 +51,37 @@ function criaDisco(disckSize, torre) {
   torre.append(novoDisk);
 }
 
+function selecionar(indice, torre) {
+  if (selecionado === 0) {
+    selecionado = indice;
+    torreSelecionada = torre;
+    torre.classList.add("selecionar");
+  } else {
+    if (selecionado !== indice) {
+      moverDisco(selecionado, indice);
+    }
+    selecionado = 0;
+    torreSelecionada.classList.remove("selecionar")
+    torreSelecionada = null;
+
+ 
+    
+  }
+}
+
 function moverDisco(t1, t2) {
   const tam1 = t1.length;
   const tam2 = t2.length;
   if (tam2 > 0 && t1[tam1 - 1] > t2[tam2 - 1]) {
-    //jogada invalida
+    moviment.textContent = "JOGADA INVALIDA"
   } else {
-    t2.push(t1.pop());
-    move = move + 1;
-    moviment.textContent = move;
+    if (t1.length !== 0) {
+      t2.push(t1.pop());
+      move = move + 1;
+      moviment.textContent = move;
+    }
   }
   render();
 }
-
 
 iniciarJogo();
